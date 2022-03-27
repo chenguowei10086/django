5da7e87fb98123290a459f390e911d9961656a5e
陈国威 <897215760@qq.com>
2022-03-27 13:43:08 +0800

    django-project

---
+++ netshop/order/urls.py
@@ -0,0 +1,16 @@
+#coding=utf-8
+
+from django.conf.urls import url
+from .import views
+
+
+
+
+
+urlpatterns=[
+    url(r'^$',views.ToOrderView.as_view()),
+    url(r'^order.html$',views.OrderListView.as_view()),
+    url(r'^topay/$',views.ToPayView.as_view()),
+    url(r'^checkPay/$',views.CheckPayView.as_view()),
+
+]
